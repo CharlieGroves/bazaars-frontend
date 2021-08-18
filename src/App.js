bfe6a1af923a75b568de84a120a75c1fc2055afb
Charlie Groves <ctrg04@gmail.com>
2021-08-18 16:46:54 +0100

    file structure changes, added routes to App.js, information from Google sign in now availible in currentUser object, created Home.js and moved it to /components folder

--- src/App.js
+++ src/App.js
@@ -1,53 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import "./css/App.css";
 import "firebase/firestore";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { firestore } from "./firebase";
-import { googleProvider, GoogleProvider } from "./context/authMethods";
-
 import { AuthenticationProvider } from "./context/AuthenticationContext";
-import { useAuth } from "./context/AuthenticationContext";
-import socialMediaAuth from "./socialMediaAuth";
+
+
+import Home from "./components/Home";
 
 function App() {
-	const messagesRef = firestore.collection("messages");
-	const [value, setValue] = useState("");
-
-	async function AddMessage(e) {
-		e.preventDefault();
-		console.log("a");
-
-		await messagesRef.add({
-			text: value,
-		});
-	}
-
-  const handleOnClick = async (provider) => {
-    const res = await socialMediaAuth(provider)
-  }
-
-
 	return (
 		<Router>
 			<AuthenticationProvider>
-					<div className="App">
-						<header className="App-header">
-              <button onClick={() => handleOnClick(googleProvider)}>Google</button>
-							<form onSubmit={AddMessage}>
-								<label>
-									Your Message: &nbsp;
-									<input
-										type="text"
-										value={value}
-										onChange={(e) =>
-											setValue(e.target.value)
-										}
-									/>
-								</label>
-								<input type="submit" name="Submit" />
-							</form>
-						</header>
-					</div>
+        <Switch>
+          <Route path='/home' component={Home} />
+          </Switch>
 			</AuthenticationProvider>
 		</Router>
 	);
---
+++ src/components/Home.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { firestore } from "../firebase";
+import { googleProvider } from "../context/authMethods";
+
+import { useAuth } from "../context/AuthenticationContext";
+import socialMediaAuth from "../socialMediaAuth";
+
+export default function Home() {
+	const messagesRef = firestore.collection("messages");
+	const [value, setValue] = useState("");
+	const { currentUser } = useAuth();
+	currentUser && console.log(currentUser);
+
+	async function AddMessage(e) {
+		e.preventDefault();
+		console.log("a");
+
+		await messagesRef.add({
+			text: value,
+		});
+	}
+
+	const handleOnClick = async (provider) => {
+		const res = await socialMediaAuth(provider);
+		console.log(res);
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<button onClick={() => handleOnClick(googleProvider)}>
+					Google
+				</button>
+                <img src={currentUser.photoURL} alt="Google Profile" />
+				<form onSubmit={AddMessage}>
+					<label>
+						Your Message: &nbsp;
+						<input
+							type="text"
+							value={value}
+							onChange={(e) => setValue(e.target.value)}
+						/>
+					</label>
+					<input type="submit" name="Submit" />
+				</form>
+			</header>
+		</div>
+	);
+}
