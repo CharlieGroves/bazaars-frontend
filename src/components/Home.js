919b2203b3bc8adf6481fcb3c7d5b3cca618b75d
Charlie Groves <ctrg04@gmail.com>
2021-08-26 21:24:13 +0100

    night commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,43 @@
 import React, { useState } from "react";
-
+import "../css/Flex.css";
 import { firestore } from "../firebase";
 import { useAuth } from "../context/AuthenticationContext";
+import {
+	useCollectionData,
+	useDocumentData,
+} from "react-firebase-hooks/firestore";
 
 export default function Home() {
 	const messagesRef = firestore.collection("messages");
-	//const shopRef = firestore.collection()
-	const [value, setValue] = useState("");
+	let uid;
+	const [shopName, setShopName] = useState("");
+	const [creatingNewShop, setCreatingNewShop] = useState(false);
 	const { currentUser, logout } = useAuth();
-	currentUser && console.log(currentUser.uid);
+	currentUser && (uid = currentUser.uid);
+	const userRef = firestore.collection("users").doc(uid);
+	const shopsRef = userRef.collection("shops");
 
-	async function AddMessage(e) {
-		e.preventDefault();
-		console.log("a");
+	const [userData] = useDocumentData(userRef);
+	const [shops] = useCollectionData(shopsRef);
 
-		await messagesRef.add({
-			text: value,
-		});
+	userData && console.log(userData.shops);
+
+	function newShopHandler() {
+		
 	}
+
+	function newShopClickHandler() {
+		setCreatingNewShop(true);
+	}
+
+	// async function AddMessage(e) {
+	// 	e.preventDefault();
+	// 	console.log("a");
+
+	// 	await messagesRef.add({
+	// 		text: value,
+	// 	});
+	// }
 
 	async function HandleLogout(e) {
 		e.preventDefault();
@@ -28,7 +48,34 @@ export default function Home() {
 		<div className="App">
 			<header className="App-header">
 				<img src={currentUser.photoURL} alt="Google Profile" />
-				<form onSubmit={AddMessage}>
+				<div className="flex-grid-master">
+					{shops &&
+						shops.map((shop, index) => (
+							<div>
+								<div>{shop.name}</div>
+								<div>{shop.description}</div>
+							</div>
+						))}
+					<button onClick={newShopClickHandler}>
+						Make a new shop
+					</button>
+				</div>
+				{creatingNewShop && (
+					<div>
+						<div>Creating new shop</div>
+						<form onSubmit={newShopHandler}></form>
+						<label>
+						Shop Name: &nbsp;
+						<input
+							type="text"
+							value={shopName}
+							onChange={(e) => setShopName(e.target.value)}
+						/>
+					</label>
+					<input type="submit" name="Submit" />
+					</div>
+				)}
+				{/* <form onSubmit={AddMessage}>
 					<label>
 						Your Message: &nbsp;
 						<input
@@ -38,8 +85,8 @@ export default function Home() {
 						/>
 					</label>
 					<input type="submit" name="Submit" />
-					<button onClick={HandleLogout}>log out</button>
-				</form>
+				</form> */}
+				<button onClick={HandleLogout}>log out</button>
 			</header>
 		</div>
 	);
