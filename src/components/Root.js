0efb94d5ecbfaa31bc92a0697d4a8ec55a64c81a
Charlie Groves <ctrg04@gmail.com>
2021-08-26 20:47:11 +0100

    cannot read uid of null fixed

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import { useAuth } from "../context/AuthenticationContext";
 import "../css/Loader.css";
@@ -11,22 +11,26 @@ export default function Root() {
 	const history = useHistory();
 	const [loading, setLoading] = useState(false);
 	const { currentUser } = useAuth();
+	const userRef = firestore.collection("users");
+
+	useEffect(async () => {
+		if (currentUser != null) {
+			const { uid, displayName } = currentUser;
+			await userRef.doc(uid).set({
+				username: displayName,
+				id: uid,
+				url: uid,
+				shops: [],
+				admin: false,
+			});
+		}
+	}, [currentUser]);
 
 	const handleSignIn = async (provider) => {
 		setLoading(true);
 		const res = await socialMediaAuth(provider);
-		const userRef = firestore.collection("users");
-		const { uid, displayName } = currentUser;
-
-		await userRef.doc(uid).set({
-			username: displayName,
-			id: uid,
-			url: uid,
-			shops: [],
-			admin: false,
-		});
-		setLoading(false)
-		//history.push("/home");
+		setLoading(false);
+		history.push("/home");
 	};
 
 	return (
