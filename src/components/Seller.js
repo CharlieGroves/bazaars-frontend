f9fca8c50a7d8d1c39c58d6aa13e1a4faa47943b
Charlie Groves <ctrg04@gmail.com>
2021-08-31 22:16:02 +0100

    /seller/:seller connected to databse

--- src/components/Seller.js
+++ src/components/Seller.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { firestore } from "../firebase";
+import {
+	useCollectionData,
+	useDocumentData,
+} from "react-firebase-hooks/firestore";
 
 export default function Seller({
 	match: {
 		params: { seller },
 	},
 }) {
+	const sellerRef = firestore.collection("users").doc(seller);
+    const [shopData] = useDocumentData(sellerRef)
+    shopData && console.log(shopData);
 	return <div>Seller {seller}</div>;
 }
