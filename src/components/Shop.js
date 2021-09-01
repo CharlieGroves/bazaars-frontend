8d31ab248622689028817b26a2340c235749a2bb
CharlieGroves <ctrg04@gmail.com>
2021-09-01 16:06:23 +0100

    shop now recognises who is the seller and who is the buyer and adjusts content accordingly

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,19 +1,35 @@
 import React from "react";
 import { firestore } from "../firebase";
-import { useCollectionData, useDocumentData } from "react-firebase-hooks/firestore";
+import {
+	useCollectionData,
+	useDocumentData,
+} from "react-firebase-hooks/firestore";
+import { useAuth } from "../context/AuthenticationContext";
 
 export default function Shop({
 	match: {
 		params: { seller, shop },
 	},
 }) {
-	const sellerRef = firestore.collection("users").doc(seller).collection(shop);
+	const sellerRef = firestore
+		.collection("users")
+		.doc(seller)
+		.collection(shop);
 	const [shopData] = useCollectionData(sellerRef);
+	const { currentUser } = useAuth();
+	let uid = "no user";
+	currentUser && (uid = currentUser.uid);
 	shopData && console.log(shopData);
 	return (
 		<div>
-			<div>Seller {seller}</div>
-			<div>Shop {shop}</div>
+			{uid != seller ? (
+				<div>
+					<div>Seller {seller}</div>
+					<div>Shop {shop}</div>
+				</div>
+			) : (
+				<div>hello seller</div>
+			)}
 		</div>
 	);
 }
