9c3ae51d8e710506a239e672d62654c44002c790
CharlieGroves <ctrg04@gmail.com>
2021-09-05 12:17:40 +0100

    shop description input added to create shop form

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,24 +12,24 @@ export default function Home() {
 	let uid;
 	let userRef;
 	let shopsRef;
+
 	const [shopName, setShopName] = useState("");
+	const [shopDescription, setShopDescription] = useState("");
 	const [creatingNewShop, setCreatingNewShop] = useState(false);
 	const { currentUser, logout } = useAuth();
+
 	currentUser && (uid = currentUser.uid);
 	currentUser && (userRef = firestore.collection("users").doc(uid));
 	currentUser && (shopsRef = userRef.collection("shops"));
-	console.log(uid);
-	currentUser && console.log(currentUser.photoURL);
-	// currentUser && console.log(photoURL)
+
 	const [userData] = useDocumentData(userRef);
 	const [shops] = useCollectionData(shopsRef);
-
-	userData && console.log(userData.shops);
 
 	const newShopHandler = async (e) => {
 		e.preventDefault();
 		await shopsRef.doc(shopName).set({
 			name: shopName,
+			description: shopDescription,
 		});
 		setShopName("");
 	};
@@ -44,53 +44,63 @@ export default function Home() {
 	}
 
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img
-					className="google-profile-photo"
-					src={currentUser.photoURL}
-					alt="Google Profile"
-				/>
-				<div className="flex-grid-master">
-					Shops: &nbsp;
-					<ul className="shop-ul">
-						{shops &&
-							shops.map((shop, index) => (
-								<li className="shop-li" key={index}>
-									<Link
-										to={`/seller/${currentUser.uid}/${shop.name}`}
-									>
-										<div>{shop.name}</div>
-										<div>{shop.description}</div>
-									</Link>
-								</li>
-							))}
-					</ul>
-					<button onClick={newShopClickHandler}>
-						Make a new shop
-					</button>
+		<div className="home-container">
+			<img
+				className="google-profile-photo"
+				src={currentUser.photoURL}
+				alt="Google Profile"
+			/>
+			<div className="flex-grid-master">
+				Shops: &nbsp;
+				<div className="shops-container">
+					{shops &&
+						shops.map((shop, index) => (
+							<div className="shops" key={index}>
+								<Link
+									to={`/seller/${currentUser.uid}/${shop.name}`}
+									className="shop-link"
+								>
+									<div className="shop-name">
+										<b>{shop.name}</b>
+									</div>
+									<div className="shop-description">
+										{shop.description}
+									</div>
+								</Link>
+							</div>
+						))}
 				</div>
-				{creatingNewShop && (
-					<div>
-						<div>Creating new shop</div>
-						<form onSubmit={newShopHandler}>
-							<label>
-								Shop Name: &nbsp;
-								<input
-									type="text"
-									value={shopName}
-									onChange={(e) =>
-										setShopName(e.target.value)
-									}
-								/>
-							</label>
-							<button type="submit">Create</button>
-						</form>
-					</div>
-				)}
-				<br />
-				<button onClick={HandleLogout}>log out</button>
-			</header>
+				<button onClick={newShopClickHandler}>Make a new shop</button>
+			</div>
+			{creatingNewShop && (
+				<div>
+					<div>Creating new shop</div>
+					<form onSubmit={newShopHandler}>
+						<label>
+							Shop Name: &nbsp;
+							<input
+								type="text"
+								value={shopName}
+								onChange={(e) => setShopName(e.target.value)}
+							/>
+						</label>
+						<br />
+						<label>
+							Shop Name: &nbsp;
+							<input
+								type="text"
+								value={shopDescription}
+								onChange={(e) =>
+									setShopDescription(e.target.value)
+								}
+							/>
+						</label>
+						<button type="submit">Create</button>
+					</form>
+				</div>
+			)}
+			<br />
+			<button onClick={HandleLogout}>log out</button>
 		</div>
 	);
 }
