20fe7c6b19b61147b5112522825134e7e0f01770
CharlieGroves <ctrg04@gmail.com>
2021-09-12 15:20:20 +0100

    SignInWithGoogleButton.js created

--- code/src/components/Welcome.js
+++ code/src/components/Welcome.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useHistory } from "react-router";
+import { Link } from "react-router-dom";
+import GoogleButton from "react-google-button";
+
+import { googleProvider } from "../context/authMethods";
+import socialMediaAuth from "../socialMediaAuth";
+import SignInWithGoogleButton from "./SignInWithGoogleButton";
+
+import "../css/Buttons.css";
+import "../css/Loader.css";
+import "../css/SignIn.css";
 
 export default function Welcome() {
-    return (
-        <div>
-            Welcome
-        </div>
-    )
+	const history = useHistory();
+	const [loading, setLoading] = useState(false);
+	return (
+		<div className="welcome-container">
+            
+			<SignInWithGoogleButton
+				loading={loading}
+				setLoading={setLoading}
+				socialMediaAuth={socialMediaAuth}
+				history={history}
+				GoogleButton={GoogleButton}
+				googleProvider={googleProvider}
+			/>
+			<Link to="signin">Sign In </Link>
+			Welcome
+		</div>
+	);
 }
