d4c377d04d431b05ed0eb2008d2f5726505a7f7f
CharlieGroves <ctrg04@gmail.com>
2021-09-20 16:46:33 +0100

    custom urls fixed

--- src/components/SignInWithGoogleButton.js
+++ src/components/SignInWithGoogleButton.js
@@ -1,26 +1,61 @@
 import React from "react";
-import { useAuth } from "../context/AuthenticationContext";
 import { firestore } from "../firebase";
 
 export default function SignInWithGoogleButton(props) {
+	const {
+		loading,
+		setLoading,
+		socialMediaAuth,
+		history,
+		GoogleButton,
+		googleProvider,
+	} = props;
 
-    const {loading, setLoading, socialMediaAuth, history, GoogleButton, googleProvider} = props;
-	const userRef = firestore.collection("users")
-	const currentUser = useAuth();
+	const userRef = firestore.collection("users");
+	const urlsRef = firestore.collection("urls");
 
-    const handleSignIn = async (provider) => {
+	const handleSignIn = async (provider) => {
 		setLoading(true);
 		const res = await socialMediaAuth(provider);
 		console.log(res);
-		const { uid, displayName, photoURL } = currentUser;
-		await userRef.doc(displayName).set({
-			username: displayName,
-			id: uid,
-			url: displayName,
-			shops: [],
-			admin: false,
-			photoURL: photoURL,
-		});
+		const { uid, displayName, photoURL } = res;
+		const urlsDocRef = firestore.collection("urls").doc(displayName);
+		const userDocRef = firestore.collection("users").doc(uid)
+		userDocRef.get().then(async (docSnapshot) => {
+			if (docSnapshot.exists) {
+				return;
+			} else {
+				// user doesn't exist yet
+				// create user document
+				await userRef.doc(uid).set({
+					username: displayName,
+					id: uid,
+					url: displayName,
+					shops: [],
+					admin: false,
+					photoURL: photoURL,
+				});
+				urlsDocRef.get().then(async (docSnapshot) => {
+					if (docSnapshot.exists) {
+						// if someone with same username exists, make the new user's
+						// url to be their uid which is unique
+						await urlsRef.doc(uid).set({
+							url: uid,
+							uid: uid,
+						});
+					} else {
+						// if this is the first user with this username, make the new
+						// user's url to be their name
+						await urlsRef.doc(displayName).set({
+							url: displayName,
+							uid: uid,
+						});
+					}
+				});
+			}
+		})
+		
+		
 		setLoading(false);
 		history.push("/home");
 	};
