125934decb7fc78bb7d3a6c32071d5c5d430995f
root <root@CharlieDesktop.localdomain>
2022-02-09 21:32:06 +0000

    to laptop;

--- src/components/ManageShop.js
+++ src/components/ManageShop.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import SingleBar from "./SingleBar";
+import "../css/ManageShop.css";
+import ShopItem from "./shopItem";
 
 export default function ManageShop({
 	match: {
@@ -24,16 +26,18 @@ export default function ManageShop({
 	let max = 300;
 	const [itemsData, setItemsData] = useState();
 	const [shopData, setShopData] = useState();
+	const [bestItemData, setBestItemData] = useState();
+	const [totalRevenue, setTotalRevenue] = useState(0);
 	useEffect(() => {
-		// axios
-		// 	.get(process.env.REACT_APP_BACKEND_IP + "get/item/" + shop)
-		// 	.then((response) => {
-		// 		setItemsData(response.data);
-		// 		// console.log(response.data);
-		// 	})
-		// 	.catch((error) => {
-		// 		console.log(error.response.status);
-		// 	});
+		axios
+			.get(process.env.REACT_APP_BACKEND_IP + "get/item/" + shop)
+			.then((response) => {
+				setItemsData(response.data);
+				// console.log(response.data);
+			})
+			.catch((error) => {
+				console.log(error.response.status);
+			});
 		axios
 			.get(process.env.REACT_APP_BACKEND_IP + "get/shop/name/" + shop)
 			.then((response) => {
@@ -43,46 +47,75 @@ export default function ManageShop({
 			.catch((error) => {
 				console.log(error.response.status);
 			});
+		axios
+			.get(process.env.REACT_APP_BACKEND_IP + "get/best/" + shop)
+			.then((response) => {
+				setBestItemData(response.data);
+				console.log(response.data.sales);
+			})
+			.catch((error) => {
+				console.log(error.response.status);
+			});
 	}, []);
 
 	useEffect(() => {
-		shopData &&
-			(() => {
-				for (let i = 0; i++; i < 12) {
-					console.log(i);
-				}
-			});
-	}, [shopData]);
+		if (shopData) {
+			for(let i = 0; i < shopData.sales_revenue.length; i++) {
+				setTotalRevenue(totalRevenue + shopData.sales_revenue[i])	
+			}
+		}
+	}, []);
 
 	return (
-		<div>
-			<div>seller {seller}</div>
-			<div>Managing {shop}</div>
-			{itemsData && <div>Number of items {itemsData.length}</div>}
-			{shopData &&
-				[...Array(12)].map((x, i) => (
-					<div key={i}>
-						sales in {monthNames[i]}&nbsp;
-						{shopData.sales[i] == null ? 0 : shopData.sales[i]}
-					</div>
-				))}
-			<div>
+		<div className="manage-shop-container">
+			<div className="manage-shop-title">
+				Welcome {seller}! You're managing {shop}:
+			</div>
+
+			{itemsData && (
+				<div>
+					<div>Total Revenue {totalRevenue}</div>
+					<div>Number of items {itemsData.length}</div>
+				</div>
+			)}
+			{bestItemData && (
+				<div>
+					<h3>Best Item:</h3>
+					{/* <p>Sales {bestItemData.sales[2]}</p> */}
+				</div>
+			)}
+			{bestItemData && <ShopItem itemsData={bestItemData} />}
+			<div className="graph-container">
+				<p style={{ fontSize: 24 }}>Sales for {shop}</p>
 				<div style={{ display: "flex" }}>
 					{shopData &&
-						[...Array(12)].map((x, i) => {
-							const y = max - (max * shopData.sales[i]) / 100;
-							return (
-								<SingleBar
-									key={i}
-									width="60px"
-									height="300px"
-									color="#ea1"
-									month={monthNames[i]}
-									number={shopData.sales[i]}
-									data={`M 0 ${max} L 0  ${y} L 60 ${y} l 60 ${max} Z`}
-								/>
-							);
-						})}
+						shopData.sales &&
+						[...Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)].map(
+							(x, i) => {
+								const y = max - (max * shopData.sales[x]) / 100;
+								console.log(x);
+								return (
+									<SingleBar
+										key={i}
+										width="50px"
+										height="300px"
+										color="#ea1"
+										month={monthNames[x]}
+										number={
+											shopData.sales
+												? shopData.sales[x]
+												: 0
+										}
+										sales={
+											shopData.sales_revenue
+												? shopData.sales_revenue[x]
+												: 0
+										}
+										data={`M 0 ${max} L 0  ${y} L 60 ${y} l 60 ${max} Z`}
+									/>
+								);
+							}
+						)}
 				</div>
 			</div>
 		</div>
--- src/components/SingleBar.js
+++ src/components/SingleBar.js
@@ -1,30 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function SingleBar({ width, height, data, color, month, number }) {
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center"
-      }}
-    >
-      <svg style={{ marginLeft: 5 }} width={width} height={height}>
-        <path
-          style={{
-            animation: "bounce linear 400ms",
-            transformOrigin: "50% 100%",
-            margin: "auto"
-          }}
-          d={data}
-          fill={color}
-        />
-      </svg>
-      <p style={{ fontSize: 12 }}>{month}</p>
-      <p style={{ fontSize: 12 }}>{number}</p>
-    </div>
-  );
+function SingleBar({ width, height, data, color, month, number, sales }) {
+	return (
+		<div
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+			}}
+		>
+			<svg style={{ marginLeft: 5 }} width={width} height={height}>
+				<path
+					style={{
+						animation: "bounce linear 400ms",
+						transformOrigin: "50% 100%",
+						margin: "auto",
+					}}
+					d={data}
+					fill={color}
+				/>
+			</svg>
+			<p style={{ fontSize: 12 }}>{month}</p>
+			<p style={{ fontSize: 12 }}>{number}</p>
+			{sales ? <p style={{ fontSize: 12 }}>£{sales}</p> : <p style={{ fontSize: 12 }}>No data</p>}
+		</div>
+	);
 }
 
 export default SingleBar;
