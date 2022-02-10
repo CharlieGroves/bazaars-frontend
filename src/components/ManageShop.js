4ef9d0870291a80885f30532640beb00c0439305
root <root@laptop.localdomain>
2022-02-10 17:00:08 +0000

    to desltop

--- src/components/ManageShop.js
+++ src/components/ManageShop.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import SingleBar from "./SingleBar";
 import "../css/ManageShop.css";
 import ShopItem from "./shopItem";
+import { Link } from "react-router-dom";
 
 export default function ManageShop({
 	match: {
@@ -27,17 +28,18 @@ export default function ManageShop({
 	const [itemsData, setItemsData] = useState();
 	const [shopData, setShopData] = useState();
 	const [bestItemData, setBestItemData] = useState();
+	const [worstItemData, setWorstItemData] = useState();
 	const [totalRevenue, setTotalRevenue] = useState(0);
 	useEffect(() => {
-		axios
-			.get(process.env.REACT_APP_BACKEND_IP + "get/item/" + shop)
-			.then((response) => {
-				setItemsData(response.data);
-				// console.log(response.data);
-			})
-			.catch((error) => {
-				console.log(error.response.status);
-			});
+		// axios
+		// 	.get(process.env.REACT_APP_BACKEND_IP + "get/item/" + shop)
+		// 	.then((response) => {
+		// 		setItemsData(response.data);
+		// 		// console.log(response.data);
+		// 	})
+		// 	.catch((error) => {
+		// 		console.log(error.response.status);
+		// 	});
 		axios
 			.get(process.env.REACT_APP_BACKEND_IP + "get/shop/name/" + shop)
 			.then((response) => {
@@ -51,6 +53,17 @@ export default function ManageShop({
 			.get(process.env.REACT_APP_BACKEND_IP + "get/best/" + shop)
 			.then((response) => {
 				setBestItemData(response.data);
+				console.log(response.data);
+				console.log(response.data.sales);
+			})
+			.catch((error) => {
+				console.log(error);
+				console.log(error.response.status);
+			});
+		axios
+			.get(process.env.REACT_APP_BACKEND_IP + "get/worst/" + shop)
+			.then((response) => {
+				setWorstItemData(response.data);
 				console.log(response.data.sales);
 			})
 			.catch((error) => {
@@ -59,32 +72,76 @@ export default function ManageShop({
 	}, []);
 
 	useEffect(() => {
-		if (shopData) {
-			for(let i = 0; i < shopData.sales_revenue.length; i++) {
-				setTotalRevenue(totalRevenue + shopData.sales_revenue[i])	
-			}
+		console.log(shopData);
+		shopData &&
+			shopData.sales_revenue &&
+			console.log(shopData.sales_revenue);
+		shopData && shopData.sales_revenue && getShopTotal();
+	}, [shopData]);
+
+	function getShopTotal() {
+		let total = 0;
+		for (let i = 0; i < 12; i++) {
+			total = total + shopData.sales_revenue[i];
+			console.log(shopData.sales_revenue[i]);
+			console.log(total);
 		}
-	}, []);
+		return setTotalRevenue(total);
+	}
 
 	return (
 		<div className="manage-shop-container">
 			<div className="manage-shop-title">
-				Welcome {seller}! You're managing {shop}:
+				Welcome {seller}! You're managing {shop}
 			</div>
 
-			{itemsData && (
-				<div>
-					<div>Total Revenue {totalRevenue}</div>
-					<div>Number of items {itemsData.length}</div>
+			<div className="details">
+				{shopData && shopData.sales_revenue && (
+					<div>Total Revenue £{totalRevenue}</div>
+				)}
+				{itemsData && <div>Number of items {itemsData.length}</div>}
+			</div>
+			<div className="best-and-worst-container">
+				<div className="best-container">
+					{bestItemData && (
+						<div>
+							<h3>Best Item:</h3>
+							{/* <p>Sales {bestItemData.sales[2]}</p> */}
+						</div>
+					)}
+					{bestItemData && (
+						<div className="best-item">
+							<div className="item-image-container">
+								<img
+									alt={bestItemData.name}
+									className="item-image"
+									src={bestItemData.itemImageURL}
+								/>
+							</div>
+							<Link
+								to={`/seller/${bestItemData.sellerId}/${bestItemData.shopName}/${bestItemData.itemName}`}
+								className="item-link"
+							>
+								{bestItemData.itemName}:
+								<br />
+							</Link>
+							<div className="item-price">
+								£{bestItemData.itemPrice}
+							</div>
+						</div>
+					)}
 				</div>
-			)}
-			{bestItemData && (
 				<div>
-					<h3>Best Item:</h3>
-					{/* <p>Sales {bestItemData.sales[2]}</p> */}
+					{worstItemData && (
+						<div>
+							<h3>Worst Item:</h3>
+							{/* <p>Sales {worstItemData.sales[2]}</p> */}
+						</div>
+					)}
+					{worstItemData && <ShopItem itemsData={worstItemData} />}
 				</div>
-			)}
-			{bestItemData && <ShopItem itemsData={bestItemData} />}
+			</div>
+
 			<div className="graph-container">
 				<p style={{ fontSize: 24 }}>Sales for {shop}</p>
 				<div style={{ display: "flex" }}>
